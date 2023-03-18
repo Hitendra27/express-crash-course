1dea5c0c31de305fa49a326ed0e20d0860c64bda
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2023-03-18 23:03:04 +0000

    added Handlebars

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init middleware
 //app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members,
+  })
+);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
